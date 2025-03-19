0024640b47884e2cb403d54a15d05002db3a7044
AnLaxina <anilovlaxina@gmail.com>
2025-03-19 16:45:40 -0700

    Add CSS styling to bundler

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "development",
@@ -7,5 +9,22 @@ module.exports = {
         filename: "output.js",
         path: path.resolve(__dirname, "dist"),
         clean: true
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/html/template.html"]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/html/template.html"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
     }
 };
