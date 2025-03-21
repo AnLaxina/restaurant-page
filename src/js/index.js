50a694fd120d31783770b39fd6c29ac7a4bdfd50
AnLaxina <anilovlaxina@gmail.com>
2025-03-21 12:17:23 -0700

    Add event listener to the home button that loads the home page whenever it is clicked

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,10 @@ import "../css/styles.css";
 import loadHomePage from "./home.js";
 
 loadHomePage();
+
+// Retrieve the 3 menu buttons
+const homeButton = document.querySelector("#home");
+const menuButton = document.querySelector("#menu");
+const aboutButton = document.querySelector("#about");
+
+homeButton.addEventListener("click", loadHomePage);
