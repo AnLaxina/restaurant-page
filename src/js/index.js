be85f83c0d9dc94ae36d720953a0b01ecf552806
AnLaxina <anilovlaxina@gmail.com>
2025-03-20 17:12:54 -0700

    Finish adding skeleton for the home menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
 import { coolGreeting } from "./greeting.js";
 import "../css/styles.css";
+import CoffeeImage from "../img/coffee-clipart.jpg";
 console.log(coolGreeting);
+
+const figure = document.querySelector("figure");
+
+// for creating images
+const coffeeImage = document.createElement("img");
+coffeeImage.src = CoffeeImage;
+figure.prepend(coffeeImage)
