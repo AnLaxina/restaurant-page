b1b1b4ae6967df708ff84160a0ec59794c55912f
AnLaxina <anilovlaxina@gmail.com>
2025-03-24 20:13:32 -0700

    Add working functionality for applying styles to the current tab

--- src/js/index.js
+++ src/js/index.js
@@ -11,8 +11,21 @@ const menuButton = document.querySelector("#menu");
 const aboutButton = document.querySelector("#about");
 
 homeButton.addEventListener("click", loadHomePage);
+homeButton.addEventListener("click", (e) => {
+    highlightTabButton(e);
+})
 menuButton.addEventListener("click", loadMenuPage);
+menuButton.addEventListener("click", (e) => {
+    highlightTabButton(e);
+})
 aboutButton.addEventListener("click", loadAboutPage);
-
-
-
+aboutButton.addEventListener("click", (e) => {
+    highlightTabButton(e);
+})
+// Helper function to add styling to the button based
+// on the currently visited page
+function highlightTabButton(event) {
+    const allTabButtons = document.querySelectorAll("button");
+    allTabButtons.forEach((button) => button.classList.replace("highlighted", "normal"));
+    event.target.classList.replace("normal", "highlighted");
+}
