52d04ee500c425d672745bc2ed000204b69d28f4
AnLaxina <anilovlaxina@gmail.com>
2025-03-24 19:21:47 -0700

    Use template literals instead of concatanation to make it more readable

--- src/js/menu.js
+++ src/js/menu.js
@@ -10,33 +10,33 @@ export default function loadMenuPage() {
 
 // Helper function to add all HTML code into a String
 function generateMenuHTML() {
-    var contentToAdd = "";
-    contentToAdd += "<h1>Cozy Bites - Menu<\/h1>";
-    contentToAdd += "";
-    contentToAdd += "        <h2>The Menu<\/h2>";
-    contentToAdd += "";
-    contentToAdd += "        <div class=\"menu-item\" id=\"cozy-burger\">";
-    contentToAdd += "            <figure>";
-    contentToAdd += "                <figcaption>$7.00: Signature Cozy Burger<\/figcaption>";
-    contentToAdd += "            <\/figure>";
-    contentToAdd += "            <p>Introducing our greatest burger! With a juicy beef patty with melted chedder,";
-    contentToAdd += "                caramelized onions, and house sauce.";
-    contentToAdd += "            <\/p>";
-    contentToAdd += "        <\/div>";
-    contentToAdd += "        <div class=\"menu-item\" id=\"mac-cheese\">";
-    contentToAdd += "            <figure>";
-    contentToAdd += "                <figcaption>$5.00: Homemade Mac & Cheese<\/figcaption>";
-    contentToAdd += "            <\/figure>";
-    contentToAdd += "            <p>Creamy, cheesy goodness with a crispy breadcrumb topping.";
-    contentToAdd += "            <\/p>";
-    contentToAdd += "        <\/div>";
-    contentToAdd += "        <div class=\"menu-item\" id=\"pancakes\">";
-    contentToAdd += "            <figure>";
-    contentToAdd += "                <figcaption>$9.00: Cozy Pancakes<\/figcaption>";
-    contentToAdd += "            <\/figure>";
-    contentToAdd += "            <p>Fluffy pancakes with a hint of vanilla, served with our homemade maple syrup.";
-    contentToAdd += "            <\/p>";
-    contentToAdd += "        <\/div>";
+    const contentToAdd = `<h1>Cozy Bites - Menu</h1>
+
+        <h2>The Menu</h2>
+
+        <div class="menu-item" id="cozy-burger">
+            <figure>
+                <figcaption>$7.00: Signature Cozy Burger</figcaption>
+            </figure>
+            <p>Introducing our greatest burger! With a juicy beef patty with melted chedder,
+                caramelized onions, and house sauce.
+            </p>
+        </div>
+        <div class="menu-item" id="mac-cheese">
+            <figure>
+                <figcaption>$5.00: Homemade Mac & Cheese</figcaption>
+            </figure>
+            <p>Creamy, cheesy goodness with a crispy breadcrumb topping.
+            </p>
+        </div>
+        <div class="menu-item" id="pancakes">
+            <figure>
+                <figcaption>$9.00: Cozy Pancakes</figcaption>
+            </figure>
+            <p>Fluffy pancakes with a hint of vanilla, served with our homemade maple syrup.
+            </p>
+        </div>`;
+
     return contentToAdd;
 
 }
